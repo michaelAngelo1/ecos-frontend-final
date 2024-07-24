0172236935d54b05e0ef7956c876793420db055b
johe lappy <johevin.blesstowi@binus.ac.id>
2024-07-25 05:33:53 +0700

    wanna go chat

--- app/(tabs)/chats.tsx
+++ app/(tabs)/chats.tsx
@@ -1,58 +1,32 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { styles } from '../config/Fonts'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { ScrollView, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../config/Fonts";
+import useGetToken from "@/hooks/useGetToken";
+import useGetUserData from "@/hooks/useGetUserData";
+import ModalLoading from "@/components/ModalLoading";
 
 const Chats = () => {
+  const { token } = useGetToken();
+  const user = useGetUserData(token);
 
-  const getToken = async () => {
-    try {
-      const userToken = await AsyncStorage.getItem("userToken")
-      if (userToken !== null) {
-        console.log("User token read: ", userToken);
-        return userToken;
-      }
-    } catch (e) {
-      console.log("Error reading JWT", e);
-    }
-  } 
-
-  /* 
-    TODO:
-    1. Get token stored on AsyncStorage
-    2. Get user detail by token
-    3. Get ROLE from user detail
-    4. If ROLE == 'Passenger', show Passenger Chatpage
-    5. Else if ROLE == 'Partner', show Partner Chatpage
-  */
-  
-  // dummy state role variable, replace LATER
-  let role = 'Passenger'; // Partner or Passenger
+  if (user.loading) {
+    return <ModalLoading />;
+  }
 
   return (
-    <SafeAreaView className='bg-[#fff] h-full'>
-      {
-        role == 'Passenger' ? 
-        <>
-          <Text className='text-2xl text-black mt-4 ml-5' style={styles.montserratBold}>Messages</Text>
-          <ScrollView>
-            <View className='flex flex-col min-h-[100vh] justify-start items-start px-4'>
+    <SafeAreaView className="bg-[#fff] h-full">
+      <Text
+        className="text-2xl text-black mt-4 ml-5"
+        style={styles.montserratBold}
+      >
+        Messages
+      </Text>
+      <ScrollView>
+        <View className="flex flex-col min-h-[100vh] justify-start items-start px-4"></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-
-              
-
-              
-            </View>
-          </ScrollView>
-        </>
-        :
-        <>
-          <Text>Partner Chatpage</Text>
-        </>
-      }
-    </SafeAreaView>    
-  )
-}
-
-export default Chats
+export default Chats;

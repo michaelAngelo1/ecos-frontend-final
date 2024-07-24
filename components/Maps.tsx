fbb5617745d55dede135a752a2cf889144d0d222
unknown <michaelangelo4489@gmail.com>
2024-07-24 14:59:46 +0700

    feat: maps connected to Google

--- components/Maps.tsx
+++ components/Maps.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import MapView from 'react-native-maps';
 import { View } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Maps = () => {
   return (
     <View>
-      <MapView className='w-full h-full'/>
+      <MapView 
+        className='w-full h-full'
+        provider={PROVIDER_GOOGLE}
+      />
     </View>
   )
 }
